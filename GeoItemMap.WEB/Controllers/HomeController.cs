7412f000f07633ea825c84af078c1b55b4c92dfa
Daniil <33331403+MineLisovich@users.noreply.github.com>
2023-05-16 14:44:10 +0300

    123

--- GeoItemMap.WEB/Controllers/HomeController.cs
+++ GeoItemMap.WEB/Controllers/HomeController.cs
@@ -7,20 +7,32 @@ using GeoItemMap.DAL.Interfaces;
 
 namespace GeoItemMap.WEB.Controllers
 {
+    
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
         private IGenericRepository<GeoItem> GeoItemGenericRepository;
-        public HomeController(ILogger<HomeController> logger , IGenericRepository<GeoItem> _GeoItemGenericRepository)
+        private IGenericRepository<Technopark> TechnoparkGenericRepository;
+        private IGenericRepository<InnProject> InnProgectGenericRepository;
+        private IGenericRepository<NtpProject> NtpProjectGenericRepository;
+        private IGenericRepository<TechTransferCenter> TechTransferCenterGenericRepository;
+
+        public HomeController(ILogger<HomeController> logger , IGenericRepository<GeoItem> _GeoItemGenericRepository, 
+            IGenericRepository<Technopark> _TechnoparkGenericRepository, IGenericRepository<InnProject> _InnGenericRepository,
+            IGenericRepository<NtpProject> _NtpGenericRepository, IGenericRepository<TechTransferCenter> _TTCgenericRepository)
         {
             _logger = logger;     
             GeoItemGenericRepository = _GeoItemGenericRepository;
+            TechnoparkGenericRepository = _TechnoparkGenericRepository;
+            InnProgectGenericRepository = _InnGenericRepository;
+            NtpProjectGenericRepository = _NtpGenericRepository;
+            TechTransferCenterGenericRepository = _TTCgenericRepository;
         }
 
         public IActionResult Index()
         {
-            return RedirectToAction(nameof(GetMapYandexApi2));
+            return RedirectToAction(nameof(GetMapYandexApi3));
         }
 
         public IActionResult GetMapYandexApi2()
@@ -41,7 +53,39 @@ namespace GeoItemMap.WEB.Controllers
             return Json(model);
         }
 
-    
+        [HttpGet]
+        public async Task <IEnumerable<GeoItem>> GetGeoCoordinates()
+        {
+            IEnumerable<GeoItem> model = await GeoItemGenericRepository.GetAll(); 
+            return model;
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> GetAllData ()
+        {  
+            var innProj = await InnProgectGenericRepository.GetAll();
+            var ntpProj = await NtpProjectGenericRepository.GetAll();
+            var tehPark = await TechnoparkGenericRepository.GetAll();
+            var ttc = await TechTransferCenterGenericRepository.GetAll();   
+            GeoItemViewModel geoItemViewModel = new GeoItemViewModel
+            {
+                InnProjects = innProj,
+                NtpProjects = ntpProj,
+                Technoparks = tehPark,
+                TechTransferCenter = ttc
+            };
+            return Json(geoItemViewModel);  
+        }
+
+        //[HttpGet]
+        //public async Task<IEnumerable<Technopark>> GetGeoItems()
+        //{
+        //    IEnumerable<Technopark> model = await TechnoparkGenericRepository.GetAll();
+
+            
+        //    return model;
+        //}
+
 
         public IActionResult Privacy()
         {
--- GeoItemMap.WEB/Program.cs
+++ GeoItemMap.WEB/Program.cs
@@ -12,6 +12,10 @@ var configuration = builder.Configuration;
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IGenericRepository<GeoItem>, EFGenericRepository<GeoItem>>();
+builder.Services.AddScoped<IGenericRepository<Technopark>, EFGenericRepository<Technopark>>();
+builder.Services.AddScoped<IGenericRepository<InnProject>, EFGenericRepository<InnProject>>();
+builder.Services.AddScoped<IGenericRepository<NtpProject>, EFGenericRepository<NtpProject>>();
+builder.Services.AddScoped<IGenericRepository<TechTransferCenter>, EFGenericRepository<TechTransferCenter>>();
 builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(@"Data Source=PC11-1203-3N\SQLEXPRESS;  Database=GeoItemDB;  Persist Security Info =false; User='sa'; Password='sa'; MultipleActiveResultSets=True; Trusted_Connection=False;"));
 
 var app = builder.Build();
