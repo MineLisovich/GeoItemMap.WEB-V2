16bc0bbc947b3e0757d1deb63897ce458442dc0c
Daniil <33331403+MineLisovich@users.noreply.github.com>
2023-05-19 09:26:20 +0300

    map design

--- GeoItemMap.WEB/Controllers/HomeController.cs
+++ GeoItemMap.WEB/Controllers/HomeController.cs
@@ -38,11 +38,7 @@ namespace GeoItemMap.WEB.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
