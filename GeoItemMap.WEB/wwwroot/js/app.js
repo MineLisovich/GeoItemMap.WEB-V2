1b442da28cd54a1487e9a869ea948d11e9e187f8
Daniil <33331403+MineLisovich@users.noreply.github.com>
2023-06-01 10:18:15 +0300

    фильтрация меток

--- GeoItemMap.WEB/wwwroot/js/app.js
+++ GeoItemMap.WEB/wwwroot/js/app.js
@@ -1,10 +1,11 @@
-import '../css/vendors/bootstrap.min.css'
+
 import '../sass/abstracts/_variables.scss'
 import '../sass/base/_settings.scss'
-import '../sass/components/_layer_button.scss'
 import '../sass/components/_media.scss'
 import '../sass/components/_modals.scss'
-import '../sass/pages/_footer.scss'
-import '../sass/pages/_header.scss'
+import '../sass/components/_layer_button.scss'
 import '../sass/pages/_sidebarmap.scss'
+import '../sass/pages/_header.scss'
+import '../sass/pages/_footer.scss'
+import '../css/vendors/bootstrap.min.css'
 
