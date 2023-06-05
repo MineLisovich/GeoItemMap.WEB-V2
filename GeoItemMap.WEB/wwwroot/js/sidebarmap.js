f415749f604683547bc09e65a3163d70a335036b
Klimovich <diana_klimovich@bk.ru>
2023-06-05 15:02:32 +0300

    unification

--- GeoItemMap.WEB/wwwroot/js/sidebarmap.js
+++ GeoItemMap.WEB/wwwroot/js/sidebarmap.js
@@ -134,47 +134,47 @@ document.querySelectorAll("ol li > div").forEach(div => {
 
 
 //modal window for the gallery
-function openModal() {
-    document.getElementById("modal-gallery").style.display = "block";
-    showSlides(slideIndex); // Показать первый слайд при открытии модального окна
-  }
+//function openModal() {
+//    document.getElementById("modal-gallery").style.display = "block";
+//    showSlides(slideIndex); // Показать первый слайд при открытии модального окна
+//  }
 
-  function closeModal() {
-    document.getElementById("modal-gallery").style.display = "none";
-  }
+//  function closeModal() {
+//    document.getElementById("modal-gallery").style.display = "none";
+//  }
 
-  var slideIndex = 1;
-  showSlides(slideIndex);
+//  var slideIndex = 1;
+//  showSlides(slideIndex);
 
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+//  function plusSlides(n) {
+//    showSlides(slideIndex += n);
+//  }
 
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+//  function currentSlide(n) {
+//    showSlides(slideIndex = n);
+//  }
 
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slides-gallery");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
-  }
+//  function showSlides(n) {
+//    var i;
+//    var slides = document.getElementsByClassName("slides-gallery");
+//    var dots = document.getElementsByClassName("demo");
+//    var captionText = document.getElementById("caption");
+//    if (n > slides.length) {
+//      slideIndex = 1;
+//    }
+//    if (n < 1) {
+//      slideIndex = slides.length;
+//    }
+//    for (i = 0; i < slides.length; i++) {
+//      slides[i].style.display = "none";
+//    }
+//    for (i = 0; i < dots.length; i++) {
+//      dots[i].className = dots[i].className.replace(" active", "");
+//    }
+//    slides[slideIndex - 1].style.display = "block";
+//    dots[slideIndex - 1].className += " active";
+//    captionText.innerHTML = dots[slideIndex - 1].alt;
+//  }
 
 
   //multiple-select 
