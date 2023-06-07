06c816f309001da77dc99b6dc547efdfe5190e35
Daniil <33331403+MineLisovich@users.noreply.github.com>
2023-06-07 10:18:05 +0300

    js code refactoring

--- GeoItemMap.WEB/wwwroot/js/header.js
+++ GeoItemMap.WEB/wwwroot/js/header.js
@@ -1,47 +1,15 @@
-
-
-
-        //О проекте 
-        function aboutProject() {
-            document.getElementById("new-about-cont").classList.toggle("show_block");
-            console.log("fwsfwsf")
-        }
-
-        //Закрыть окно О проекте
-            $('.about_project_close_button').click(function() {
-                document.getElementById("new-about-cont").classList.remove("show_block");
-            }
-        );
-
-
-        //Инструменты
-        function measurementTools() {
-            document.getElementById("itsystem-measure-menu").classList.toggle("show_block");
-        }
-
-
-        
-        window.onclick = function(event) {
-            if (!event.target.matches('.itsystem-curr-lang')) {
-                var dropdowns = document.getElementsByClassName("itsystem-lang-menu");
-                var i;
-                for (i = 0; i < dropdowns.length; i++) {
-                    var openDropdown = dropdowns[i];
-                    if (openDropdown.classList.contains('show_block')) {
-                        openDropdown.classList.remove('show_block');
-                    }
-                }
-            }
-
-            if (!event.target.matches('.itsystem-tool-button')) {
-                var dropdowns = document.getElementsByClassName("itsystem-measure-menu");
-                var i;
-                for (i = 0; i < dropdowns.length; i++) {
-                    var openDropdown = dropdowns[i];
-                    if (openDropdown.classList.contains('show_block')) {
-                        openDropdown.classList.remove('show_block');
-                    }
-                }
-            }
-        }
+$(document).ready(function () {
+    // О проекте (открыть модальное окно)
+    const $aboutProject_open = $("#aboutProject_open");
+    $aboutProject_open.click(function () {
+        $("#new-about-cont").addClass("show_block");
+    });
+    // О проекте (закрыть модальное окно)
+    const aboutProject_close = $("#aboutProject_close");
+    aboutProject_close.click(function () {
+        $("#new-about-cont").removeClass("show_block");
+    });
+});
+       
+      
 
