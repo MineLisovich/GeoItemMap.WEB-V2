478f022f9bc36e4d1e3e06f603d2baa235c67db6
Daniil <33331403+MineLisovich@users.noreply.github.com>
2023-06-12 09:27:28 +0300

    Revert "Revert "new design YMAP""
    This reverts commit 845e69bb96398430f2c5f479b315a35f51d73c3b.

--- GeoItemMap.DAL/DataContext/AppDbContext.cs
+++ GeoItemMap.DAL/DataContext/AppDbContext.cs
@@ -5,14 +5,11 @@ namespace GeoItemMap.DAL.DataContext
 {
     public class AppDbContext : DbContext
     {
-
-
         public class OptionsBuild
         {
             public DbContextOptionsBuilder<AppDbContext> opsBuilder { get; set; }
             public DbContextOptions<AppDbContext> dbOptins { get; set; }
             private DALConfiguration _appConfiguration { get; set; }
-
             public OptionsBuild()
             {
                 _appConfiguration = new DALConfiguration();
@@ -24,15 +21,11 @@ namespace GeoItemMap.DAL.DataContext
         public static OptionsBuild ops = new OptionsBuild();
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbContextOptions<AppDbContext> ConnectionString { get; }
-
         public DbSet<GeoItem>? GeoItems { get; set; }
-
         public DbSet <NtpProject>? NtpProjects { get; set; }
         public DbSet <InnProject>? InnProjects { get; set; }  
         public DbSet <Technopark>? Technoparks { get; set;}
-
         public DbSet <TechTransferCenter>? Techtransfercenters { get; set;}
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<GeoItem>().ToTable("GeoItems");
@@ -40,7 +33,6 @@ namespace GeoItemMap.DAL.DataContext
             modelBuilder.Entity<InnProject>().ToTable("InnProjects");
             modelBuilder.Entity<Technopark>().ToTable("Technoparks");
             modelBuilder.Entity<TechTransferCenter>().ToTable("TechTransferCenters");
-
 
             modelBuilder.Entity<Technopark>().HasData(new Predefined.PredefinedTechnoparkList().Technoparks);
             modelBuilder.Entity<TechTransferCenter>().HasData(new Predefined.PredefinedTechTransferCenterList().TechTransferCenters);
